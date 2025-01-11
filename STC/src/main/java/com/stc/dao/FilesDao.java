c2116a14fda10dd0091fac38e838258f7d97b750
s.mahmoud.c <smahmoud.c@saip.gov.sa>
2025-01-11 22:02:56 +0200

    remove

--- STC/src/main/java/com/stc/dao/FilesDao.java
+++
@@ -1,9 +0,0 @@
-package com.stc.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.stc.entity.Files;
-
-public interface FilesDao extends JpaRepository<Files, Long>{
-
-}
